ea10d92ee8507a9f5120ee53913499932c880869
ostre <ostre@softserveinc.com>
2021-12-01 17:09:09 +0200

    Added login page missing links

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'page/login'
+  post 'page/login'
   devise_for :users
   get 'page/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
