34bd35a6671f772520e04abbc54ca51096083cc2
ostre <ostre@softserveinc.com>
2021-12-02 16:34:29 +0200

    Added Devise controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
   get 'page/login'
   post 'page/login'
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
+  devise_scope :user do
+    get 'page/sign_in', to: 'devise/sessions#new'
+  end
+
+
   get 'page/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
