2c67b5ee912d84859de507dc9f264659e1e44888
ostre <ostre@softserveinc.com>
2021-12-03 15:41:21 +0200

    Fixed router

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
-  get 'page/login'
-  post 'page/login'
+  root 'page#home'
+
+  get 'login', to: 'page#login'
+  post 'login', to: 'page#login'
   devise_for :users, controllers: { sessions: 'users/sessions' }
   devise_scope :user do
     get 'page/sign_in', to: 'devise/sessions#new'
   end
 
 
-  get 'page/home'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get 'home', to: 'page#home'
 end
