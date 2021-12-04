349181654a1b6d6f268a592aa70add073d7e39f5
ostre <ostre@softserveinc.com>
2021-12-04 22:18:01 +0200

    Added Category model and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_29_141553) do
+ActiveRecord::Schema.define(version: 2021_12_04_201616) do
+
+  create_table "categories", charset: "utf8mb3", force: :cascade do |t|
+    t.string "title"
+    t.text "desc"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
     t.string "email", default: "", null: false
