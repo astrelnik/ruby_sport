b8e1751ddd48f1097f8a8389f8ae7bbdb04296ef
ostre <ostre@softserveinc.com>
2021-12-15 23:21:28 +0200

    Added mailer feature

--- db/schema.rb
+++ db/schema.rb
@@ -39,10 +39,10 @@ ActiveRecord::Schema.define(version: 2021_12_14_174408) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.string "first_name"
-    t.string "last_name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
