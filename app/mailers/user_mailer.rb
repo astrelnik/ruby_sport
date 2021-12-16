710f3b528d55faca3f530e4e4051a5cf1bd4ca5a
ostre <ostre@softserveinc.com>
2021-12-16 23:25:16 +0200

    Added mailer config

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,12 @@
 class UserMailer < ApplicationMailer
-  default from: 'alisterr1987@gmail.com'
+  default from: 'alisterrr1987@gmail.com'
 
   def welcome_email
     @user = params[:user]
     @url  = 'http://example.com/login'
-    mail(to: @user.email, subject: 'Welcome to My Awesome Site')
+    mail(to: @user.email,
+         subject: 'Welcome to My Awesome Site',
+         template_path: 'notifications',
+         template_name: 'another')
   end
 end
