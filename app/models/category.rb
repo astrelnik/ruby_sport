78a0f301eb5402a72957a62bb2986ab1851790bc
ostre <ostre@softserveinc.com>
2022-01-11 16:35:16 +0200

    Added category title getter from subcategory object

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,5 +2,5 @@ class Category < ApplicationRecord
   validates :title, presence: true, length: {minimum: 4}
   validates :desc, presence: true, length: {minimum: 4}
 
-  has_many :subcategory, dependent: :destroy
+  has_many :subcategories, dependent: :destroy
 end
--- app/models/subcategory.rb
+++ app/models/subcategory.rb
@@ -1,4 +1,4 @@
 class Subcategory < ApplicationRecord
   belongs_to :category
-
+  delegate :title, to: :category, prefix: true
 end
