757138eec720c85ca52ef27711189f91ed3e7313
ostre <ostre@softserveinc.com>
2022-01-13 15:32:13 +0200

    Added create article rspec test

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,4 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
+  subject {
+    described_class.new(
+      title: 'Some article',
+      desc: 'Lorem ipsum',
+      published_at: DateTime.now,
+      category_id: 1
+    )
+  }
+  it 'creates article' do
+    expect(subject).to be_valid
+  end
 end
