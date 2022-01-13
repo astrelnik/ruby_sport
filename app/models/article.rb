1af8de883f900a9a4476bd210efd946309aac5bd
ostre <ostre@softserveinc.com>
2022-01-13 15:29:01 +0200

    Fixed bug with create article with empty category

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ApplicationRecord
   validates :title, presence: true, length: {minimum: 5}
   validates :desc, presence: true, length: {minimum: 5}
 
-  belongs_to :category
+  belongs_to :category, optional: true
   delegate :title, to: :category, prefix: true, allow_nil: true
 
   def self.getPublished
