c8699325699c0c01515184331429e66ee45cb573
ostre <ostre@softserveinc.com>
2022-01-14 15:33:28 +0200

    Added model tests

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
+  it 'has many' do
+    should have_many(:subcategories)
+  end
 end
--- spec/models/subcategory_spec.rb
+++ spec/models/subcategory_spec.rb
@@ -1,4 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Subcategory, type: :model do
+
+  it 'belongs_to' do
+    should belong_to :category
+  end
 end
