24b33038df40624100c2f18ee0072aaabec789b2
ostre <ostre@softserveinc.com>
2022-01-15 22:44:34 +0200

    Added articles controller test

--- spec/requests/articles_spec.rb
+++ spec/requests/articles_spec.rb
@@ -1,6 +1,10 @@
 require 'rails_helper'
 
-RSpec.describe "Articles", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Articles', type: :request do
+  describe 'GET /articles' do
+    it 'returns a successful articles response' do
+      get articles_path
+      expect(response).to be_successful
+    end
   end
 end
